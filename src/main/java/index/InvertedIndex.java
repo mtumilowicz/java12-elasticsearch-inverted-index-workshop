6ab1b333cc8f3287c51a684be63ab3e66c8c0e8e
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-04-06 19:28:39 +0200

    rebuilding scoring draft 2

--- src/main/java/index/InvertedIndex.java
+++ src/main/java/index/InvertedIndex.java
@@ -1,25 +1,31 @@
 package index;
 
-import document.Document;
 import document.DocumentId;
-import lombok.AllArgsConstructor;
 import tokenizer.Token;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class InvertedIndex {
 
-    Map<Token, IndexEntries> index = new HashMap<>();
+    Map<Token, Set<DocumentId>> index = new HashMap<>();
+    Map<Token, Map<DocumentId, Frequency>> stats = new HashMap<>();
 
     public void put(Token token, DocumentId documentId) {
-        index.computeIfAbsent(token, ignore -> new IndexEntries()).put(documentId, token);
+        index.computeIfAbsent(token, ignore -> new HashSet<>()).add(documentId);
+        stats.computeIfAbsent(token, ignore -> new HashMap<>())
+                .computeIfAbsent(documentId, ignore -> Frequency.zero())
+                .increment();
 
     }
 
     public Stream<Yyy> get(Token token) {
-        return index.get(token).stream();
+        return index.getOrDefault(token, new HashSet<>())
+                .stream()
+                .map(x -> new Yyy(x, stats.get(token).get(x), new Frequency(index.get(token).size())));
     }
 
 }
